780c65fb24baf07927eadfa0d6da7b3baa8ccdc3
edgardo90 <edgardo9000@gmail.com>
2022-08-27 06:09:56 -0300

    creacion del banner y modificacion navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http" // esto lo importo yo , es para que funcione el service
+import {FormsModule} from "@angular/forms" // esto lo importo yo , para los formularios
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LogosComponent } from './components/logos/logos.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    LogosComponent
+    LogosComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule, // lo traigo aca
+    FormsModule, // lo importo aca
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
