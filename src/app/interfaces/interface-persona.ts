8be6d17499b5d6e08e707be93a5cf0b18a534ae5
edgardo90 <edgardo9000@gmail.com>
2022-08-27 15:25:01 -0300

    creacion service de Persona y limpieza de codigo logos, navBar del html

---
+++ src/app/interfaces/interface-persona.ts
@@ -0,0 +1,12 @@
+export interface Persona{
+    id?:number,
+    name:string,
+    surname:string,
+    imagen:string,
+    phone:string,
+    email:string,
+    country:string,
+    userName:string,
+}
+
+export const PersonArray : Persona[] = []
