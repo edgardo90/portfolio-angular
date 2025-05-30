51619b18d30be2e98281d3e3dd3e407c0924840b
Edgardo Gonzalez <86448044+edgardo90@users.noreply.github.com>
2025-05-30 04:04:36 -0300

    Update contact.service.ts

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -14,7 +14,7 @@ const httpOption = { // esto es para decir que envio un json a la aplicacion , "
   providedIn: 'root'
 })
 export class ContactService {
-  private contactUrl = "https://quixotic-ong-edgardo90.koyeb.app/contacts/portfolio"; // ruta que sirve para enviar por email automatico 
+  private contactUrl = "https://back-portofolio-java-v1-0.onrender.com/portfolio"; // ruta que sirve para enviar por email automatico 
 
   constructor(private http:HttpClient) { }
 
